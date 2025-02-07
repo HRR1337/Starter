8528174453f3df2550f1e80b789dc40d6fc0f507
Shift <shift@laravelshift.com>
2025-02-07 11:56:26 +0000

    Adopt Laravel type hints

--- database/migrations/2024_02_04_131050_add_themes_settings_to_users_table.php
+++ database/migrations/2024_02_04_131050_add_themes_settings_to_users_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('theme')->nullable()->default('default');
@@ -14,7 +14,7 @@ return new class extends Migration
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['theme', 'theme_color']);
